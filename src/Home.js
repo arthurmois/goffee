b6ea275af11b0d4428d6a6b01d7a79210330ce28
Arthur Mois <arthur8564@gmail.com>
2021-12-26 20:01:27 -0800

    respnsive to mobile

--- src/Home.js
+++ src/Home.js
@@ -1,24 +1,29 @@
 import { Button, Image, Page } from "@geist-ui/react";
 import { Text } from "@geist-ui/react";
 
-export default function Home() {
+import { useState, useContext } from "react";
 
+import useMediaQuery from "./useMediaQuery";
+
+
+export default function Home() {
+  let isPageSmall = useMediaQuery("(max-width: 600px)");
+  const pageWidth = isPageSmall ? "100%" : "80%";
   return (
-    <Page>
+    <Page width={pageWidth}>
       <Page.Header>
         <Text h1>Goffee</Text>
         <Text h5 style={{ color: 'grey' }}>Treat others to coffee, virtually!</Text>
       </Page.Header>
       <Page.Content>
         <Text h5 height={7}>
-          Goffee provides an easy way for you to send coffee deliveries to your peers and employees.<br/>
-          With a curated link and a code word formed by you, those you wish to send coffee to can customize their order and<br/>
-          simply provide their address with instructions and receive a coffee, on the house! Well... on your house ;)
+          Goffee provides an easy way for you to send coffee deliveries to your peers and employees.<br/><br/>
+          With a curated link and a code word formed by you, those you wish to send coffee to can customize their order receive a coffee, on the house! Well... on your house ;)
         </Text>
         <Image height='450px' src="https://images.unsplash.com/photo-1606509036992-4399d5c5afe4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c2hhcmluZyUyMGNvZmZlZXxlbnwwfHwwfHw%3D&w=1000&q=80"/>
-        <br />
         <Button shadow type="secondary" onClick={() => { window.location.href = 'https://form.jotform.com/213596847839072' }}>Order Now</Button>
       </Page.Content>
+      <br/>
       <Page.Footer>
         <Text h6>Have Suggestions? Contact us at email.com</Text>
       </Page.Footer>
